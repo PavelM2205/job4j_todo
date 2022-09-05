100d7a7317db4191222d2fc527199750ad6d2bd8
PavelM2205 <pavelm2205@mail.ru>
2022-09-05 18:39:40 +0700

    Добавлен класс Main

---
+++ src/main/java/ru/job4j/Main.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        SessionFactory sf = new MetadataSources(registry).buildMetadata()
+                .buildSessionFactory();
+        return sf;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("Go to: http://localhost:8080/index");
+    }
+}
