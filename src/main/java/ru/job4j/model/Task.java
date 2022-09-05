35b2ce4bdd6b8367f0a43682f555da48bb3e5150
PavelM2205 <pavelm2205@mail.ru>
2022-09-05 18:42:03 +0700

    Внесены изменения в класс Task

--- src/main/java/ru/job4j/model/Task.java
+++ src/main/java/ru/job4j/model/Task.java
@@ -1,13 +1,31 @@
 package ru.job4j.model;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "tasks")
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Task {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String description;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MM-EEEE-yyyy HH:mm:ss");
     private boolean done;
+
+    @Override
+    public String toString() {
+        return "Task{"
+                + "id=" + id + ", description='" + description + '\''
+                + ", created=" + FORMATTER.format(created)
+                + ", done=" + done + '}';
+    }
 }
